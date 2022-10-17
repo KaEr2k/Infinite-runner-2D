e85b06e066e07ccea7cd3cb7d33e9a3d5bbda9a9
Flepnee <114177457+Flepnee@users.noreply.github.com>
2022-10-17 22:57:39 +0200

    Add files via upload

--- Assets/Movement.cs
+++ Assets/Movement.cs
@@ -4,51 +4,38 @@ using UnityEngine;
 
 public class Movement : MonoBehaviour
 {
-    public float speed = 1f;
-    public Vector3 newPosition, lerpStartPos;
-    public float lerpTime = .5f;
-    public float cooldownv2 = 0f;
-    [SerializeField] private float cooldown = 0.1f;
+    public float moveSpeed = 7f;
+    public LayerMask obstacleLayers;
+    public Transform movePoint;
+    public Animator anim;
     // Start is called before the first frame update
     void Start()
     {
-        
+        movePoint.parent = null;
     }
 
-
     // Update is called once per frame
-    void Update() {
-        cooldownv2 -= Time.deltaTime;
+    void Update()
+    {
+        transform.position = Vector3.MoveTowards(transform.position, movePoint.position, moveSpeed * Time.deltaTime);
 
-        if ((Input.GetKeyDown(KeyCode.A) || Input.GetKeyDown(KeyCode.LeftArrow)) && 0 >= cooldownv2) {
-            lerpStartPos = transform.position;
-            newPosition = transform.position + -Vector3.right * 10;
-            lerpTime = 0f;
-            cooldownv2 = cooldown;
-        }
-        if ((Input.GetKeyDown(KeyCode.D) || Input.GetKeyDown(KeyCode.RightArrow)) && 0 >= cooldownv2) {
-            lerpStartPos = transform.position;
-            newPosition = transform.position + Vector3.right * 10;
-            lerpTime = 0f;
-            cooldownv2 = cooldown;
-        }
-        if ((Input.GetKeyDown(KeyCode.W) || Input.GetKeyDown(KeyCode.UpArrow)) && 0 >= cooldownv2) {
-            newPosition = transform.position + Vector3.up * 10;
-            lerpStartPos = transform.position;
-           lerpTime = 0f;
-           cooldownv2 = cooldown;
-        }
-        if ((Input.GetKeyDown(KeyCode.S) || Input.GetKeyDown(KeyCode.DownArrow)) && 0 >= cooldownv2) {
-            newPosition = transform.position + -Vector3.up * 10;
-            lerpStartPos = transform.position;
-            lerpTime = 0f;
-            cooldownv2 = cooldown;
-        }
-
-        if (lerpTime < .1f) {
-            lerpTime += Time.deltaTime;
-        }
+        if (Vector3.Distance(transform.position, movePoint.position) <= 0.05f)
+        {
+            if (Mathf.Abs(Input.GetAxisRaw("Horizontal")) > 0)
+            {
+                if (!Physics2D.OverlapCircle(movePoint.position + new Vector3(Input.GetAxisRaw("Horizontal"),0f), .2f, obstacleLayers))
+                {
+                    movePoint.position += new Vector3(Input.GetAxisRaw("Horizontal"),0f);
+                }
         
-        transform.position = Vector3.Lerp(lerpStartPos, newPosition, lerpTime);
+            }
+            if (Mathf.Abs(Input.GetAxisRaw("Vertical")) > 0)
+            {
+                if (!Physics2D.OverlapCircle(movePoint.position + new Vector3(0f, Input.GetAxisRaw("Vertical")), .2f, obstacleLayers))
+                {
+                    movePoint.position += new Vector3(0f, Input.GetAxisRaw("Vertical"));
+                }
+            }
+        }
     }
 }
