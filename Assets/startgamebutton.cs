e0178a1ac94ad8913459c4274d38948a6caf519e
KaEr <70764751+KaEr2005@users.noreply.github.com>
2022-12-13 21:46:03 +0100

    Add files via upload

---
+++ Assets/startgamebutton.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class startgamebutton : MonoBehaviour
+{   // build number of scene to start when start button is pressed
+    public int gameStartScene;
+
+    public void StartGame()
+    {
+        SceneManager.LoadScene(gameStartScene);
+    }
+
+}
